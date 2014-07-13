72f751c9e9ad8c537384f67febbd9f657bb9d47a
Darren Schwarz <darrenschwarz@gmail.com>
2014-07-13 10:48:51 +0100

    created windows service

--- DNS.SignalR.Host/Program.cs
+++ DNS.SignalR.Host/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace DNS.SignalR.Host
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
         {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
