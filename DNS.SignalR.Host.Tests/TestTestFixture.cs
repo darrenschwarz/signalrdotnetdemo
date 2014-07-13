c8acc4660e681e06056a84b00ea4874c183b3175
Darren Schwarz <darrenschwarz@gmail.com>
2014-07-13 03:10:57 +0100

    failing test

---
+++ DNS.SignalR.Host.Tests/TestTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DNS.SignalR.Host.Tests
+{
+    [TestFixture]
+    public class TestTestFixture
+    {
+        [Test]
+        public void TestsNothing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
