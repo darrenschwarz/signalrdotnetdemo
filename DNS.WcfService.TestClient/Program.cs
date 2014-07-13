806908062f665f844e588ef07a16e5fe70bb2ac0
Darren Schwarz <darrenschwarz@gmail.com>
2014-07-13 23:39:08 +0100

    test

--- DNS.WcfService.TestClient/Program.cs
+++ DNS.WcfService.TestClient/Program.cs
@@ -24,7 +24,7 @@ namespace DNS.WcfService.TestClient
 
                     DoSomething(service);
 
-                    Console.WriteLine("Did something...");
+                    Console.WriteLine("Did something....");
                     Console.ReadKey(true);
 
                 }
