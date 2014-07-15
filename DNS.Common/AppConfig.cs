80c472e3d04bef0ddab207c23bac5bb78818547d
Darren Schwarz <darrenschwarz@gmail.com>
2014-07-15 12:10:55 +0100

    Adding TestData nuspec project

---
+++ DNS.Common/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+
+namespace DNS.Common
+{
+    public static class AppConfig
+    {
+        public static T GetAppSetting<T>(string key)
+        {
+            var rawValue = ConfigurationManager.AppSettings[key];
+
+            if (rawValue == null)
+                throw new ConfigurationErrorsException(
+                    string.Format
+                        ("Please configure '{0}' in the AppSettings section of the application .config file.", key));
+
+            if (typeof(T) == typeof(Uri))
+                return (T)(object)new Uri(rawValue);
+
+            var value = (T)Convert.ChangeType(rawValue, typeof(T));
+            return value;
+        }
+    }
+}
--- DNS.SignalR.Host.Tests/TestTestFixture.cs
+++ DNS.SignalR.Host.Tests/TestTestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DNS.Common;
 using NUnit.Framework;
 
 namespace DNS.SignalR.Host.Tests
@@ -6,6 +7,19 @@ namespace DNS.SignalR.Host.Tests
     [TestFixture]
     public class TestTestFixture
     {
+        private string _mongoHost;
+        public string MongoHost
+        {
+            get { return _mongoHost ?? (_mongoHost = AppConfig.GetAppSetting<string>("MongoHost")); }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            //if (!MongoRestorer.Restore(MongoHost, MongoDatabaseName, "Data"))
+            //    throw new Exception("Problem encountered restoring MongoDb.");
+        }
+
         [Test]
         public void TestsNothing()
         {
