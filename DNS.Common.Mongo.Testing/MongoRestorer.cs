0b9b8e565ce720a97180505e22d81e190f9d538e
Darren Schwarz <darrenschwarz@gmail.com>
2014-07-15 16:12:55 +0100

    Adding in Mongo Testing

---
+++ DNS.Common.Mongo.Testing/MongoRestorer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MrSite.Common.Mongo.Testing
+{
+    public class MongoRestorer
+    {
+        public static bool Restore(string host, string database, string dataDir)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(host)) throw new ArgumentNullException("host");
+                if (string.IsNullOrEmpty(database)) throw new ArgumentNullException("database");
+                if (string.IsNullOrEmpty(dataDir)) throw new ArgumentNullException("database");
+
+                if (!DataDirectoryExists(dataDir))
+                    throw new DirectoryNotFoundException("Data directory not found.");
+
+                if (!DataDirectoryHasContent(dataDir))
+                    throw new Exception("Data directory is empty.");
+
+                var startInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = false,
+                    UseShellExecute = false,
+                    FileName = "mongorestore.exe",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    Arguments = string.Format("--drop --host {0} -d {1} {2}", host, database, dataDir)
+                };
+
+
+                using (var exeProcess = Process.Start(startInfo))
+                    exeProcess.WaitForExit();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+                return false;
+            }
+        }
+
+        //TODO [Ian,20140703] test these...
+        protected static bool DataDirectoryExists(string dataDir)
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var dataDirectory = Path.Combine(baseDir, dataDir);
+            return Directory.Exists(dataDirectory);
+        }
+
+        protected static bool DataDirectoryHasContent(string dataDir)
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var dataDirectory = Path.Combine(baseDir, dataDir);
+            return Directory.GetFiles(dataDirectory).Any();
+        }
+    }
+}
--- DNS.SignalR.Host.Tests/TestTestFixture.cs
+++ DNS.SignalR.Host.Tests/TestTestFixture.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using DNS.Common;
-//using NUnit.Framework;
+using NUnit.Framework;
 
 namespace DNS.SignalR.Host.Tests
 {
-    //[TestFixture]
+    [TestFixture]
     public class TestTestFixture
     {
         private string _mongoHost;
@@ -13,23 +13,29 @@ namespace DNS.SignalR.Host.Tests
             get { return _mongoHost ?? (_mongoHost = AppConfig.GetAppSetting<string>("MongoHost")); }
         }
 
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    //if (!MongoRestorer.Restore(MongoHost, MongoDatabaseName, "Data"))
-        //    //    throw new Exception("Problem encountered restoring MongoDb.");
-        //}
+        private string _mongoDatabaseName;
+        public string MongoDatabaseName
+        {
+            get { return _mongoDatabaseName ?? (_mongoDatabaseName = AppConfig.GetAppSetting<string>("MongoDatabaseName")); }
+        }
 
-        //[Test]
-        //public void TestsNothing()
-        //{
-        //    Assert.IsTrue(true);
-        //}
+        [SetUp]
+        public void SetUp()
+        {
+            //if (!MongoRestorer.Restore(MongoHost, MongoDatabaseName, "Data"))
+            //    throw new Exception("Problem encountered restoring MongoDb.");
+        }
 
-        //[Test]
-        //public void TestsMoreNothing()
-        //{
-        //    Assert.IsTrue(true);
-        //}
+        [Test]
+        public void TestsNothing()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestsMoreNothing()
+        {
+            Assert.IsTrue(true);
+        }
     }
 }
